4eb8e8090dba93eaa446fd56256986bc805026ed
anshwhitehtjr <69585432+anshwhitehtjr@users.noreply.github.com>
2021-12-09 13:43:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App'; 
 
 ReactDOM.render(
-  <React.StrictMode baseName='/iNotebook-React'>
+  <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
